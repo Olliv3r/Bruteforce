1e092deff07611338c7b60fb83f0a9609d281e20
Olliv3r <oliveobom100@gmail.com>
2024-06-06 03:04:04 -0400

    $

--- Login/login.php
+++ Login/login.php
@@ -10,16 +10,16 @@ include_once("includes/header_html.php");
 ?>
 <div class="container">
   <form action="logar.php" method="POST">
-    <h2>Login</h2>
+    <h2>Log In</h2>
     
     <div class="form-group">
-      <input type="text" name="username" placeholder="Username" required/>
-      <label>Username</label>
+      <input type="text" name="username" required/>
+      <label>Usuário</label>
     </div>
     
     <div class="form-group">
-      <input type="password" name="password" placeholder="Password" required/>
-      <label>Password</label>
+      <input type="password" name="password" required/>
+      <label>Senha</label>
     </div>
     
     <input type="submit" name="btn_logar" value="Login"/>
